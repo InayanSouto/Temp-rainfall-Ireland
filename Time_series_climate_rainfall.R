115c4edf18c079b160361663b8d99226d6599032
Richard Curran <52069802+RichardGartan@users.noreply.github.com>
2020-03-02 00:33:01 +0000

    Add files via upload

---
+++ Time_series_climate_rainfall.R
@@ -0,0 +1,312 @@
+#### time series: altering the research question due to poor prediction quality of linear regression
+#
+
+library(tseries)
+library(forecast)
+
+#####ULSTER####
+# converting Ulster temp to time series
+Ulster_test <- slice(Ulster, 1:(n()-3))
+
+ts_Ulster_Temp <- ts(Ulster_test$`Malin Head Temp`, start = c(1990,1),
+                     frequency = 12)
+ts_Ulster_Temp
+plot(ts_Ulster_Temp, main = "Ulster raw time series")
+par(resetPar())
+# there seems to be some seasonal effect and random fluctuations
+# but no overall trend which is good 
+y_range <- c(min(ts_Ulster_Temp), max(ts_Ulster_Temp))
+y_range
+par(mfrow = c(3,1))
+plot(ma(ts_Ulster_Temp, 3), main = "Simple moving avg (k=3)",
+     ylim = y_range)
+plot(ma(ts_Ulster_Temp, 7), main = "Simple moving avg (k=7)",
+     ylim = y_range)
+plot(ma(ts_Ulster_Temp, 15), main = "Simple moving avg (k=15)",
+     ylim = y_range)
+
+#ma smooths the time seires. as k increases so does smoothness
+
+adf.test(ts_Ulster_Temp)
+# p value less than 0.05: time series is stationary
+# and there is no trend
+ndiffs(ts_Ulster_Temp)
+
+# confirms that nothing needs to be done to the series
+# and that it is stationary i.e series does not need
+# to be differenced
+par(mfrow = c(2,1))
+Pacf(ts_Ulster_Temp, main = "partial auto correlation plot for Ulster Temp")
+Acf(ts_Ulster_Temp, main = "auto correlation plot for Ulster temp")
+par(resetPar())
+#there are loads of different p and q values that can be chosen,
+# best to use auto ARIMA instead of cycling through each
+# set manually
+
+#data has to be seasonally decomposed first
+# take the log of the series to convert from multiplicative
+# to additive
+log_Ulster <- log(ts_Ulster_Temp)
+plot(log_Ulster)
+
+# stl function to dempose the series
+# "period" so the seasonal components remain the same
+# across each year
+season_decomp <- stl(log_Ulster, s.window = "period")
+plot(season_decomp)
+
+# components to be reverted back to original metric
+# take the exponential of the logged series
+ts_Ulster_temp_converted <- exp(season_decomp$time.series)
+plot(ts_Ulster_temp_converted)
+ts_Ulster_temp_converted
+# example (Ulster): sep 2017 saw a 37% seasonal change 
+
+# seasonadj removes the seasonalitiy   ULSTER
+ulster_seasonadj <- seasadj(season_decomp)
+
+seasonplot(ts_Ulster_Temp, 12, col=rainbow(12),
+           year.labels =  TRUE,
+           main = "seasonal plot of ulster temp")
+##outlier in september 2013; temperature was very low compared to every other
+# year. this indicates either the influence of an unseen variable
+# or it is an error in the data. we'll see how it effects the series
+
+seasonplot(ulster_seasonadj, 12, col=rainbow(12),
+           year.labels =  TRUE,
+           main = "seasonal adjusted ulster temp")
+
+ndiffs(ulster_seasonadj)
+# d value will be 0
+adf.test(ulster_seasonadj)
+#stationary, regardless of the mad outlier in 2013 september
+
+# using auto arima to find the best model
+# there were too many options for the p and q values
+# 1,0,0 appears to be the best one
+aa_ulster <- auto.arima((ulster_seasonadj))
+aa_ulster
+qqnorm(aa_ulster$residuals, main = "Ulster residuals normality plot")
+qqline(aa_ulster$residuals)
+
+# trying a different arima model to compare accuracy
+arima_ulster <- Arima(ts_Ulster_Temp, order = c(1,0,1))
+arima_ulster
+
+accuracy(aa_ulster)
+accuracy(arima_ulster)
+#MAPE is a much lower for auto arima model. its the best one 
+# to use
+
+# testing the model fit
+# H0 =autocorrelations are all zero
+# but in this example p>0.05 and so H0 is rejected
+# the arima model is good fit for the data
+Box.test(aa_ulster$residuals, type = "Ljung-Box")
+
+forecast_U <- forecast(aa_ulster, 3)
+forecaset_U
+# not far off the actual values. Good model
+# outlier must be accounted for 
+plot(forecast_U, main = "Ulster forecast")
+
+forecast_U <- data.frame(forecast(aa_ulster, 3))
+actiual_vs_pred_U <- data.frame(actual = Ulster$`Malin Head Temp`[349:351],
+                                predicted = forecast_U$Point.Forecast)
+actiual_vs_pred_U
+# after reading met erieann weather report for 2013 September,
+# it appears there was no obvious cause to the outlier (low temperature)
+# this is likely an error in the data, and not caused by some unseen variable
+
+# replacing the outlier with the median value; distribution is not normally
+# distributed
+
+## Ulster_test$`Malin Head Temp`[285] = median(Ulster$`Malin Head Temp`)
+# this was done and it made no difference to the quality of the predictions.
+#
+
+
+
+############ CONNACHT #################
+
+#removing the last 3 rows from the original dataframe
+# predict these 3 and compare the predicted to actual
+Connacht_test <- slice(Connaucht, 1:(n()-3))
+
+ts_Connacht_Temp <- ts(Connacht_test$Mean_Temp, start = c(1990,1),
+                       frequency = 12)
+plot(ts_Connacht_Temp, main = "Raw time series")
+par(resetPar())
+# there seems to be some seasonal effect and random fluctuations
+# but no overall trend which is good 
+ts_Connacht_Temp 
+
+adf.test(ts_Connacht_Temp)
+# p value less than 0.05: time series is stationary
+# and there is no trend
+
+ndiffs(ts_Connacht_Temp)
+# confirms that nothing needs to be done to the series
+# and that it is stationary i.e series does not need
+# to be differenced
+
+Pacf(ts_Connacht_Temp, main = "partial auto correlation plot for Connacht temp")
+Acf(ts_Connacht_Temp, main = "auto correlation plot for Connacht temp")
+#there are loads of different p and q values that can be chosen,
+# best to use auto ARIMA instead of cycling through each
+# set manually
+
+#data has to be seasonally decomposed first
+# take the log of the series to convert from multiplicative
+# to additive
+
+log_Connacht <- log(ts_Connacht_Temp)
+plot(log_Connacht)
+# stl function to dempose the series
+# "period" so the seasonal components remain the same
+# across each year
+
+season_decomp_conn <- stl(log_Connacht, s.window = "period")
+plot(season_decomp_conn)
+# components to be reverted back to original metric
+# take the exponential of the logged series
+
+ts_conn_converted <- exp(season_decomp_conn$time.series)
+plot(ts_conn_converted)
+# example (Ulster): sep 2017 saw a 37% seasonal change 
+
+# seasonadj removes the seasonalitiy
+connacht_seasonadj <- seasadj(season_decomp_conn)
+
+seasonplot(ts_Connacht_Temp, 12, col = rainbow(12),
+           year.labels = TRUE, 
+           main = "seasonal plot of connacht temp")
+
+seasonplot(connacht_seasonadj, 12, col = rainbow(12),
+           year.labels = TRUE, 
+           main = "seasonal adjusted plot of connacht temp")
+#massive outlier again for this dataset. this time in december 2010
+# this will effect the prediction
+ts_Connacht_Temp
+
+ndiffs(connacht_seasonadj)
+# no differencing needed
+
+adf.test(connacht_seasonadj)
+# p value < 0.05
+# good model fit 
+
+aa_connacht <- auto.arima(connacht_seasonadj)
+aa_connacht
+
+qqnorm(aa_connacht$residuals, main = "Connacht temp normality plot")
+qqline(aa_connacht$residuals)
+# the outlier has caused problems
+# need to sort it out
+# replace this value with median/mean
+# depending on the distribution
+
+accuracy(aa_connacht)
+# mape of 19%
+
+forecast_C <- forecast(aa_connacht, 3)
+plot(forecast_C)
+
+forecast_C <- data.frame(forecast(aa_connacht, 3))
+actiual_vs_pred_C <- data.frame(actual = Connaucht$Mean_Temp[349:351],
+                                predicted = forecast_C$Point.Forecast)
+
+
+# the outlier for this year is explained by the met ireland website
+# which states 2009/2010 was the coldest winter in Ireland since the 1960's
+# this explains the low temperature for the months of jan and feb
+# the major outlier in december of 2010 is explained by met eireann
+# Mullingar had the coldest recorded temperature in over 50 years (Connacht)
+
+#### Munster #####
+ 
+Munster_test <-  slice(Munster, 1:(n()-3))
+
+ts_Munster_temp <- ts(Munster_test$`Valentia Temp`, 
+                      start = c(1990,1), frequency = 12)
+
+plot(ts_Munster_temp, main = "Raw time series")
+par(resetPar())
+# there seems to be some seasonal effect and random fluctuations
+# but no overall trend which is good 
+ts_Munster_temp
+
+
+adf.test(ts_Munster_temp)
+# p value less than 0.05: time series is stationary
+# and there is no trend
+
+ndiffs(ts_Munster_temp)
+# confirms that nothing needs to be done to the series
+# and that it is stationary i.e series does not need
+# to be differenced
+par(mfrow = c(2,1))
+Pacf(ts_Munster_temp, main = "partial auto correlation plot for Munster temp")
+Acf(ts_Munster_temp, main = "auto correlation plot for Munster temp")
+par(resetPar())
+#there are loads of different p and q values that can be chosen,
+# best to use auto ARIMA instead of cycling through each
+# set manually
+
+#data has to be seasonally decomposed first
+# take the log of the series to convert from multiplicative
+# to additive
+
+log_Munster <- log(ts_Munster_temp)
+plot(log_Munster)
+# stl function to dempose the series
+# "period" so the seasonal components remain the same
+# across each year
+season_decomp_munster <- stl(log_Munster, s.window = "period")
+plot(season_decomp_munster)
+# components to be reverted back to original metric
+# take the exponential of the logged series
+
+ts_munster_converted <- exp(season_decomp_munster$time.series)
+plot(ts_munster_converted)
+# example (Ulster): sep 2017 saw a 37% seasonal change 
+
+# seasonadj removes the seasonalitiy
+munster_seasonadj <- seasadj(season_decomp_munster)
+
+seasonplot(ts_Munster_temp, 12, col = rainbow(12),
+           year.labels = TRUE, 
+           main = "seasonal plot of Munster temp")
+
+seasonplot(munster_seasonadj, 12, col = rainbow(12),
+           year.labels = TRUE, 
+           main = "seasonal adjusted plot of Munster temp")
+#massive outlier again for this dataset. Again  in december 2010
+# this will effect the prediction
+
+
+ndiffs(munster_seasonadj)
+# no differencing needed
+
+adf.test(munster_seasonadj)
+# p value < 0.05
+# good model fit 
+
+aa_munster <- auto.arima(munster_seasonadj)
+aa_munster
+
+qqnorm(aa_munster$residuals, main = "Munster temp normality plot")
+qqline(aa_munster$residuals)
+# the outlier has caused problems
+# need to sort it out
+# replace this value with median/mean
+# depending on the distribution
+
+accuracy(aa_munster)
+# mape of 19%
+
+forecast_M <- data.frame(forecast(aa_munster, 3))
+actiual_vs_pred_M <- data.frame(actual = Munster$`Valentia Temp`[349:351],
+                      predicted = forecast_M$Point.Forecast)
+plot(forecast(aa_munster, 3))
+
---
+++ statistical_analysis_of_datasets.R
@@ -0,0 +1,414 @@
+library(ggpubr)
+library(pwr)
+library(dplyr)
+library(tibble)
+#install.packages("finalfit")
+library(finalfit)
+#install.packages("naniar")
+library(naniar)
+
+#importing data
+rainfall <- read.csv('rainfall.csv', 
+                     header = TRUE, 
+                     sep = ",",
+                     strip.white = TRUE,
+                     stringsAsFactors = FALSE)
+temp <- read.csv('temp.csv',
+                 header = TRUE, 
+                 sep = ",",
+                 strip.white = TRUE,
+                 stringsAsFactors = FALSE)
+str(rainfall)
+
+
+
+# removing unwanted rows; any row containging
+# a certain string is removed
+# only want the mean (given in dataset)
+temp <- temp[grepl("Mean", temp$X.1),]
+
+rainfall <- rainfall[grepl("Total", rainfall$X.1),]
+
+#merging two columns and removing old column
+temp$X.1 <- paste(temp$X, temp$X.1)
+temp <- temp[,-1]
+
+rainfall$X.1 <- paste(rainfall$X, rainfall$X.1)
+rainfall <- rainfall[,-1]
+
+# dealing with non-interger values
+temp[temp == ".."] <- NA
+
+rainfall[rainfall== ".."] <- NA
+
+
+
+# making rainfall and temp the variables in each dataframe
+# transposing the column and rows
+
+temp <-temp %>%
+  t() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  rownames_to_column("value") %>%
+  `colnames<-`(.[1,]) %>%
+  .[-1,] %>%
+  `rownames<-`(NULL)
+
+
+rainfall <-rainfall %>%
+  t() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  rownames_to_column("value") %>%
+  `colnames<-`(.[1,]) %>%
+  .[-1,] %>%
+  `rownames<-`(NULL)
+
+
+#for visualising the missing data and giving percentage that is missing
+# 7 of of the 15 weather stations have a substantial number of missing values
+# it is not known what caused these NA values
+# but they will be ignored in the analysis and will be dropped
+# imputation would be the preferred choice over dropping the variables
+#  but the variables that remain should give a decent view of the data even
+# with the missing data being dropped
+# this will of course effect the accuracy of the study
+
+par(mfrow=c(2,4))
+gg_miss_var(rainfall)
+gg_miss_var(temp)
+pct_miss(rainfall)
+pct_miss(temp)
+  
+
+#do not want to lose any data realting to the years,
+# so will swap the row and columns again in order
+# to drop the weather stations that have NA rather than
+# dopping the years as this would greatly effect
+# the granularity of the data 
+
+temp <-temp %>%
+  t() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  rownames_to_column("value") %>%
+  `colnames<-`(.[1,]) %>%
+  .[-1,] %>%
+  `rownames<-`(NULL)
+
+
+rainfall <-rainfall %>%
+  t() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  rownames_to_column("value") %>%
+  `colnames<-`(.[1,]) %>%
+  .[-1,] %>%
+  `rownames<-`(NULL)
+
+#removing na
+temp <- na.omit(temp) 
+rainfall <- na.omit((rainfall))
+
+#transposing again
+
+temp <-temp %>%
+  t() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  rownames_to_column("value") %>%
+  `colnames<-`(.[1,]) %>%
+  .[-1,] %>%
+  `rownames<-`(NULL)
+
+
+rainfall <-rainfall %>%
+  t() %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  rownames_to_column("value") %>%
+  `colnames<-`(.[1,]) %>%
+  .[-1,] %>%
+  `rownames<-`(NULL)
+
+
+# renaming variables
+colnames(temp) <- c("Month & Year", "Belmullet Temp","Valentia Temp", "Casement Temp", "Cork Temp",
+                           "Dublin Temp","Malin Head Temp", "Mullingar Temp", "Shannon Temp")
+
+colnames(rainfall) <-c("Month & Year","Belmullet Rain","Valentia Rain", "Casement Rain", "Cork Rain",
+                          "Dublin Rain","Malin Head Rain", "Mullingar Rain", "Shannon Rain")
+
+
+
+# merging the two datasets by the X.1 colum (yearly and monthly data)
+total <- merge(temp, rainfall, by = "Month & Year")
+total_data <-  merge(temp, rainfall, by = "Month & Year")
+
+total <-total_data[,-1]
+rownames(total) <- total_data[,1]
+
+total[, 2:ncol(total)] <- sapply(total[, 2:ncol(total)],
+                                 as.numeric)
+
+#####ignore##### this is for yearly data, replacing each variable with the mean for that year######
+#sorting out the years
+#only want to know averagae yearly values  for temp and rain
+# so edit each value in Month & Yeart varible to only contain the year
+# find.list <- list("X", "M01", "M02", "M03",
+#               "M04", "M05", "M06", "M07", 
+#                    "M08", "M09", "M10", "M11", "M12")
+# find.string <- paste(unlist(find.list), collapse = "|")
+# # # 
+# total_data$`Month & Year` <- gsub(find.string, replacement = "", x = total_data$`Month & Year`)
+# total_data$`Month & Year` <- as.numeric(total_data$`Month & Year`)
+#levels(total_data$`Month & Year`)
+# rm(Ulster)
+# 
+# #making the first colum the index (years and months)
+# #  <-total_data[,-1]
+# # rownames(total) <- total_data[,1]
+# # 
+# # 
+# # #new dataframe containg the average temp for each station 
+# # # convert all columns excpet first to numeric
+# # # calculate the mean values for each factor variable
+# # # making the first colum  the index (years)
+# total[, 1:ncol(total)] <- sapply(total[, 1:ncol(total)],
+#                                   as.numeric)
+# # 
+# yearly1 <- aggregate(total[,2:ncol(total)], 
+#            by = list(total_data$`Month & Year`), 
+#            FUN = mean)
+# summary(total_data)[["1st Qu."]]
+# outvals <- boxplot(Munster, plot = FALSE)$out
+# 
+# outvals
+# summary(total)
+
+# splitting into two groups; before the millenium and after
+
+# #1990s
+# nineties_avg <- yearly1 %>% 
+#   filter(Group.1 < 2000)
+# 
+# as.factor(nineties_avg$Group.1)
+# boxplot(`Malin Head Rain`~ Group.1, data = nineties_avg)
+# plot(nineties_avg$Group.1, nineties_avg$`Malin Head Rain`)
+# 
+# # 2000's
+# millenia_avg <-yearly1 %>%
+#   filter(Group.1 > 1999)
+# 
+# 
+# 
+# yearly <- yearly1[,-1]
+# rownames(yearly) <- yearly1[,1]
+# 
+# 
+
+#rainfall is the dependant variable and temperature is the independant
+# want to see if rainfall is affected by temp change
+#make dataframe numeric
+
+total_data[, 1:ncol(total)] <- sapply(total_data[, 1:ncol(total_data)],
+                                 as.numeric)
+str(total_data)
+
+# before Splitting dataset into groups by province,
+# checking each weather station data in Connaucht (only one with stations >2)
+#  they are positively skewed. from the histograms and density graph
+par(mfrow = c(2,2))
+hist(total$`Belmullet Rain`, main = "Belmullet rain hist.")
+hist(total$`Mullingar Rain`, main = "Mullingar rain hist.")
+hist(total$`Shannon Rain`, main = "Shannon rain hist.")
+
+#median is a better approximation and so will be used to
+# represent the province as a whole
+plot(density(total$`Belmullet Rain`),main = "Example of Connaucht skewness")
+abline(v =c(median(total$`Belmullet Rain`), mean(total$`Belmullet Rain`)),
+        col = c('red','blue'), lty = 2)
+
+
+# splitting dataset into data pertaining to each province
+Ulster <- total %>% select(`Malin Head Temp`,`Malin Head Rain`)
+Munster <- total %>% select(`Valentia Temp`,`Valentia Rain`)
+Leinster <- total %>% select(`Casement Temp`, `Casement Rain`,
+                             `Dublin Temp`, `Dublin Rain`)
+Connaucht <- total %>% select(`Belmullet Temp`, `Belmullet Rain`,
+                              `Mullingar Temp`, `Mullingar Rain`,
+                              `Shannon Temp`, `Shannon Rain`)
+
+#mean rain and temp for connaucht and leinster for each year
+# as they contain > 1 weather station
+# this will unfortunaetly effect granularity 
+Connaucht$Mean_Temp <- apply(Connaucht[,c("Belmullet Temp","Mullingar Temp",
+                                          "Shannon Temp")], 1, median)
+Connaucht$Mean_Rain <- apply(Connaucht[,c("Belmullet Rain", "Mullingar Rain",
+                                          "Shannon Rain")], 1, median)
+Leinster$Mean_Temp <- apply(Leinster[,c("Casement Temp", "Dublin Temp")], 1, mean)
+Leinster$Mean_Rain <- apply(Leinster[,c("Casement Rain", "Dublin Rain")], 1, mean)
+
+# mean, stdev skew and kurtosis for each province
+my_stats <- function(x, na.omit = FALSE){
+  if(na.omit)
+    x <- x[!is.na(x)] #omits missing values
+  med <- median(x)
+  m <- mean(x)
+  n <- length(x)
+  s <- sd(x)
+  skew <- sum((x-m) ^ 3 / s ^ 3)/n
+  kurt <- sum((x-m) ^ 4 / s ^ 4)/n - 3
+  
+  return(c(median= med,n = n, mean = m, stdev = s, skew = skew,
+           kurtosis = kurt))
+}
+sapply(Connaucht[,7:ncol(Connaucht)], my_stats)
+sapply(Munster, my_stats)
+sapply(Ulster, my_stats)
+sapply(Leinster[,5:ncol(Leinster)], my_stats)
+#each have kurtosis < 3 indiciating Platykurtic distribution so fewer extreme 
+# extreme values at the tails than that of normal distribution
+# further edging us toward non-parametric test
+
+#function telling how manyh outliers are present in each group
+# there are very few outliers per group comapared to the amount of data
+
+outvals <- function(x){
+    outliers <-boxplot(x, plot = FALSE)$out
+  
+
+  
+   return(sum(outliers > 0))
+
+  }
+
+sapply(Ulster, outvals)
+sapply(Connaucht, outvals)
+sapply(Munster, outvals)
+sapply(Leinster, outvals)
+
+
+#density plots and histograms for each province
+# shows releveant skewness of the dependat variable
+d_u <- density(Ulster$`Malin Head Rain`) 
+d_c <- density(Connaucht$Mean_Rain)
+d_m <- density(Munster$`Valentia Rain`)
+d_l <- density(Leinster$Mean_Rain)
+
+
+
+par(mfrow=c(2,2))
+#densit plots; using median as straight line throught the median
+# through the median because of the positive skewness
+
+plot(d_u, main = "Ulster Rain dist.")
+abline(v = 87.8,#median
+       lty = 2, col = "blue")
+plot(d_c, main = "Connaucht Rain dist.")
+abline(v =79.5 ,#median
+        col ="blue", lty = 2)
+plot(d_m, main = "Munster Rain dist.")
+abline(v = 122.6,#median
+       lty = 2, col = "blue")
+plot(d_l, main = "Leinster Rain dist.")
+abline(v = 60.3,#median
+       lty = 2, col = "blue")
+
+hist(Ulster$`Malin Head Rain`, main = "Ulster Rain hist.")
+hist(Connaucht$Mean_Rain, main = "Connaucht Rain hist.")
+hist(Munster$`Valentia Rain`, main = "Munster Rain hist.")
+hist(Leinster$Mean_Rain, main = "Leinster Rain hist.")
+# postiviely skewed; best to use median. 
+
+#effect size for the analysis is 0.3
+#  correlation is the analysis type
+cohen.ES(test = "r", size = "medium")
+
+
+# type 2 error tolerance of 0.1 so Power = 0.90
+# type 1 error tolerance is usually 0.05. anything
+# greater than this and H0 must be accepted or risk
+# makign type 1 error
+# two sided test (default); using two tail test because
+# regardless of the direction of the relationship, I am
+# testing for the possibility of a relationship in both directions
+r_test <- pwr.r.test(r = 0.3, sig.level = 0.05, power = 0.9, alternative = "two.sided")
+plot(r_test)
+
+#taking the recommended sample
+
+set.seed(1234)
+sample <- dplyr::sample_n(Ulster, 150)
+sample_c <- dplyr::sample_n(Connaucht, 150)
+sample_m <- dplyr::sample_n(Munster, 150)
+sample_l <- dplyr::sample_n(Leinster, 150)
+#normality plot
+# to see if the data follows a normal distribution.
+# central limit theorem states that a large enough sample size
+# sample > 30 will lead to a normal dist.
+
+
+
+ggqqplot(sample$`Malin Head Rain`, main ="Ulster Normality")
+
+ggqqplot(sample_c$Mean_Rain, main = "Connaucht Normality")
+
+ggqqplot(sample_m$`Valentia Rain`, main = "Munster Normality")
+
+ggqqplot(sample_l$Mean_Rain, main = "Leinster Normality")
+
+#shapiro to show normality as visually determening normality is unreliable with 
+# large dataframes
+# p value is << 0.05 meaning a non-parametric test must be used
+# also the plots shows the data does not follow a normal distribution - high kurtosis
+
+shapiro.test(sample$`Malin Head Rain`)
+shapiro.test(sample_c$Mean_Rain)
+shapiro.test(sample_m$`Valentia Rain`)
+shapiro.test(sample_l$Mean_Rain)
+
+
+# correlation test: spearman test
+
+# ulster
+cor.test(sample$`Malin Head Temp`, sample$`Malin Head Rain`,
+         method = "spearman", exact = FALSE)
+
+# munster
+cor.test(sample_m$`Valentia Temp`, sample_m$`Valentia Rain`,
+         method = "spearman", exact = FALSE)
+
+#connacht 
+cor.test(sample_c$Mean_Temp, sample_c$Mean_Rain,
+         method = "spearman", exact =FALSE)
+# 
+# #cor.test(sample_c$`Belmullet Temp`, sample_c$`Belmullet Rain`,
+#          method = "spearman", exact =FALSE)
+# 
+# cor.test(sample_c$`Mullingar Temp`, sample_c$`Mullingar Rain`,
+#          method = "spearman", exact =FALSE)
+# cor.test(sample_c$`Shannon Temp`, sample_c$`Shannon Rain`,
+#          method = "spearman", exact =FALSE)
+
+# Leinster
+cor.test(sample_l$Mean_Temp, sample_l$Mean_Rain,
+         method = "spearman", exact = FALSE)
+
+#Leinster accepts the null hypothesis as p-value > 0.05; avoiding a type 1 error
+# each of the other provinces have p valu << 0.05 and so accept the alternate hypothesis; 
+# avoiding a type 2 error (accepting H0)
+
+
+
+library(forecast)
+
+write.csv(Ulster, file = "ulster.csv")
+y_range <- c(min(sample_m$`Valentia Rain`), max(sample_m$`Valentia Rain`))
+
+plot(ma(sample_m$`Valentia Rain`, 15), ylim = y_range)
+Acf(Ulster)
+
+library(tseries)
+adf.test(Ulster)
+
+Pacf(Ulster)
+
+ndiffs(h)
+
+
